830cdd841c631189d365b659ae983af4fb5ee60b
Jonathan Harris <jonathan@marginal.org.uk>
2016-05-05 18:14:51 +0100

    Defer to LegacyMovie.qldisplay
    if LegacyMovie.qldisplay is symlinked as Movie.qldisplay and QTKit says
    it can play the track.

--- qlgenerator/GeneratePreviewForURL.m
+++ qlgenerator/GeneratePreviewForURL.m
@@ -1,12 +1,15 @@
 #import <Cocoa/Cocoa.h>
 #import <QuickLook/QuickLook.h>
 #import <AVFoundation/AVFoundation.h>
+#import <QTKit/QTKit.h>
 
 #include "snapshotter.h"
 
 // Implemented in main.m
 extern NSBundle *myBundle;
 extern BOOL brokenQLPrefetch;
+BOOL hackedQLDisplay;
+
 
 // Undocumented options
 const CFStringRef kQLPreviewOptionModeKey = CFSTR("QLPreviewMode");
@@ -82,22 +85,48 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 
         if (!thePreview)
         {
-            // If AVFoundation can play it, then hand it off to
-            // /System/Library/Frameworks/Quartz.framework/Frameworks/QuickLookUI.framework/PlugIns/Movie.qldisplay
-            AVAsset *asset = [AVAsset assetWithURL:(__bridge NSURL *)url];
-            if (asset)	// note: asset.playable==true doesn't imply there's a playable video track
+            if (hackedQLDisplay)
             {
-                AVAssetTrack *track = [asset tracksWithMediaType:AVMediaTypeVideo].firstObject;
-                if (track && track.playable)
+                // If QTKit can play it, then hand it off to
+                // /System/Library/Frameworks/Quartz.framework/Frameworks/QuickLookUI.framework/PlugIns/LegacyMovie.qldisplay symlinked as Movie.qldisplay
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+                QTMovie *movie = [QTMovie movieWithAttributes:@{QTMovieURLAttribute:(__bridge NSURL *)url,
+                                                                QTMovieOpenForPlaybackAttribute:@true,
+                                                                QTMovieOpenAsyncOKAttribute:@false}
+                                                        error:nil];
+                if (movie)
                 {
-                    QLPreviewRequestSetURLRepresentation(preview, url, contentTypeUTI, (__bridge CFDictionaryRef) properties);
-                    return kQLReturnNoError;    // early exit
+                    QTTrack *track = [movie tracksOfMediaType:QTMediaTypeVideo].firstObject;
+                    if (track)
+                    {
+                        QLPreviewRequestSetURLRepresentation(preview, url, contentTypeUTI, (__bridge CFDictionaryRef) properties);
+                        return kQLReturnNoError;    // early exit
+                    }
+                }
+#pragma clang diagnostic pop
+            }
+            else
+            {
+                // If AVFoundation can play it, then hand it off to
+                // /System/Library/Frameworks/Quartz.framework/Frameworks/QuickLookUI.framework/PlugIns/Movie.qldisplay
+                AVAsset *asset = [AVAsset assetWithURL:(__bridge NSURL *)url];
+                if (asset)	// note: asset.playable==true doesn't imply there's a playable video track
+                {
+                    AVAssetTrack *track = [asset tracksWithMediaType:AVMediaTypeVideo].firstObject;
+                    if (track && track.playable)
+                    {
+                        QLPreviewRequestSetURLRepresentation(preview, url, contentTypeUTI, (__bridge CFDictionaryRef) properties);
+                        return kQLReturnNoError;    // early exit
+                    }
                 }
             }
             if (QLPreviewRequestIsCancelled(preview)) return kQLReturnNoError;
 
             // kQLPreviewPrefetchMode is broken for "non-native" files on Mavericks - the user gets a blank window
-            // if they invoke QuickLook soon after. Presumambly QuickLookUI is caching and getting confused?
+            // if they invoke QuickLook soon after. Presumably QuickLookUI is caching and getting confused?
             // If we return nothing we get called again with no QLPreviewMode option. This somehow forces QuickLookUI to
             // correctly call us with kQLPreviewQuicklookMode when the user later invokes QuickLook. What a crock.
             if (brokenQLPrefetch && previewMode == kQLPreviewPrefetchMode)
