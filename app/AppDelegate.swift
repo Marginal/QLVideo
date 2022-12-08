c1023f52fa165b68e923cd211c0733a92cbc3fcc
Jonathan Harris <jonathan@marginal.org.uk>
2022-12-08 14:57:00 +0000

    Build for older x86_64 only. Remove AVX2 check.

--- app/AppDelegate.swift
+++ app/AppDelegate.swift
@@ -69,17 +69,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         snapshotTimeValue.stringValue = snapshotTimeFormatter.string(from: TimeInterval(snapshotTime.integerValue)) ?? "\(snapshotTime.integerValue)"
 
-        // Check if unsupported hardware and don't do further setup if so
-        if sysCtl("hw.machine") == "x86_64" && sysCtl("hw.optional.avx2_0") != "yes" {
-            let alert = NSAlert()
-            alert.alertStyle = .critical
-            alert.messageText = "QuickLook Video requires a late-2013 Mac or newer, with AVX2 support"
-            alert.informativeText = "The QuickLook and Spotlight plugins will crash!"
-            alert.addButton(withTitle: "OK")
-            alert.runModal()
-            return
-        }
-
         // Remove old plugins
         if !cleanup() {
             // Will fail if Sandboxed or if user refuses to authorize
