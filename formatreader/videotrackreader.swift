6eaaecd6d059ec74e9db4284909aedfb84a81bd3
Jonathan Harris <jonathan@marginal.org.uk>
2026-01-03 17:55:17 +0000

    Add support for motion JPEG codec

--- formatreader/videotrackreader.swift
+++ formatreader/videotrackreader.swift
@@ -44,61 +44,68 @@ class VideoTrackReader: TrackReader {
         }
 
         var atoms: CFDictionary? = nil
-        if params.extradata_size != 0 {
-            if params.codec_id == AV_CODEC_ID_MPEG4 {
-                // MPEG4 part 2 https://developer.apple.com/documentation/quicktime-file-format/mpeg-4_elementary_stream_descriptor_atom
-                // FFmpeg only retains the decoder-specific info from "esds" in extradata - so rebuild it.
-                // See videotoolbox_esds_extradata_create in https://ffmpeg.org/doxygen/8.0/videotoolbox_8c_source.html
-                let decoder_size = params.extradata_size  // assumed to be < 16K
-                let config_size = 13 + 5 + decoder_size
-                let full_size = 3 + 5 + config_size + 6
-                let bytes: [UInt8] =
-                    [
-                        0, 0, 0, 0,  // Version/flags = ESDS
-                        0x03,  // ES_DescrTag
-                        0x80, 0x80, UInt8(0x80 | (full_size >> 7)), UInt8(full_size & 0x7f),
-                        0x00, 0x01, 0,  // ES_ID, priority
-                        0x04,  // DecoderConfigDescrTag
-                        0x80, 0x80, UInt8(0x80 | (config_size >> 7)), UInt8(config_size & 0x7f),
-                        0x20, 0x11,  // MPEG 4, video
-                        0, 0, 0,  // buffer size
-                        0, 0, 0, 0,  // max bitrate
-                        0, 0, 0, 0,  // avg bitrate
-                        0x05,  // DecSpecificInfoTag
-                        0x80, 0x80, UInt8(0x80 | (decoder_size >> 7)), UInt8(decoder_size & 0x7f),
-                    ] + [UInt8](UnsafeBufferPointer<UInt8>(start: params.extradata, count: Int(params.extradata_size))) + [
-                        0x06,  // SLConfigDescrTag
-                        0x80, 0x80, 0x80, 1,
-                        0x02,
-                    ]
-                atoms = ["esds" as CFString: CFDataCreate(kCFAllocatorDefault, bytes, CFIndex(bytes.count))] as CFDictionary
-
-            } else if params.codec_id == AV_CODEC_ID_H264 {
-                // avc1 / MPEG4 part 10 https://developer.apple.com/documentation/quicktime-file-format/avc_decoder_configuration_atom
-                atoms =
-                    [
-                        "avcC" as CFString: CFDataCreateWithBytesNoCopy(
-                            kCFAllocatorDefault,
-                            params.extradata,
-                            CFIndex(params.extradata_size),
-                            kCFAllocatorNull  // extradata will be deallocated by avformat_close_input()
-                        )
-                    ] as CFDictionary
-            } else if params.codec_id == AV_CODEC_ID_HEVC {
-                // assume this works like above?
-                atoms =
-                    [
-                        "hvcC" as CFString: CFDataCreateWithBytesNoCopy(
-                            kCFAllocatorDefault,
-                            params.extradata,
-                            CFIndex(params.extradata_size),
-                            kCFAllocatorNull  // extradata will be deallocated by avformat_close_input()
-                        )
-                    ] as CFDictionary
-            } else {
-                let hex = UnsafeBufferPointer(start: params.extradata, count: Int(params.extradata_size)).map {
-                    String(format: "%02x", $0)
-                }.joined(separator: " ")
+        switch params.codec_id {
+        case AV_CODEC_ID_MJPEG:
+            // Need an esds atom, but FFmpeg doesn't provide one. Make a minimal one.
+            let bytes: [UInt8] = [
+                0, 0, 0, 0, 0x03, 0x80, 0x80, 0x80, 0x1b, 0, 0x01, 0, 0x04, 0x80, 0x80, 0x80, 0x0d, 0x6c, 0x11, 0, 0, 0, 0,
+                0x8b, 0xa5, 0xda, 0, 0x8b, 0xa5, 0xda, 0x06, 0x80, 0x80, 0x80, 0x01, 0x02,
+            ]
+            atoms = ["esds" as CFString: CFDataCreate(kCFAllocatorDefault, bytes, CFIndex(bytes.count))] as CFDictionary
+        case AV_CODEC_ID_MPEG4:
+            // MPEG4 part 2 https://developer.apple.com/documentation/quicktime-file-format/mpeg-4_elementary_stream_descriptor_atom
+            // FFmpeg only retains the decoder-specific info from "esds" in extradata - so rebuild it.
+            // See videotoolbox_esds_extradata_create in https://ffmpeg.org/doxygen/8.0/videotoolbox_8c_source.html
+            let decoder_size = params.extradata_size  // assumed to be < 16K
+            let config_size = 13 + 5 + decoder_size
+            let full_size = 3 + 5 + config_size + 6
+            let bytes: [UInt8] =
+                [
+                    0, 0, 0, 0,  // Version/flags = ESDS
+                    0x03,  // ES_DescrTag
+                    0x80, 0x80, UInt8(0x80 | (full_size >> 7)), UInt8(full_size & 0x7f),
+                    0x00, 0x01, 0,  // ES_ID, priority
+                    0x04,  // DecoderConfigDescrTag
+                    0x80, 0x80, UInt8(0x80 | (config_size >> 7)), UInt8(config_size & 0x7f),
+                    0x20, 0x11,  // MPEG 4, video
+                    0, 0, 0,  // buffer size
+                    0, 0, 0, 0,  // max bitrate
+                    0, 0, 0, 0,  // avg bitrate
+                    0x05,  // DecSpecificInfoTag
+                    0x80, 0x80, UInt8(0x80 | (decoder_size >> 7)), UInt8(decoder_size & 0x7f),
+                ] + [UInt8](UnsafeBufferPointer<UInt8>(start: params.extradata, count: Int(params.extradata_size))) + [
+                    0x06,  // SLConfigDescrTag
+                    0x80, 0x80, 0x80, 0x01,
+                    0x02,
+                ]
+            atoms = ["esds" as CFString: CFDataCreate(kCFAllocatorDefault, bytes, CFIndex(bytes.count))] as CFDictionary
+        case AV_CODEC_ID_H264:
+            // avc1 / MPEG4 part 10 https://developer.apple.com/documentation/quicktime-file-format/avc_decoder_configuration_atom
+            atoms =
+                [
+                    "avcC" as CFString: CFDataCreateWithBytesNoCopy(
+                        kCFAllocatorDefault,
+                        params.extradata,
+                        CFIndex(params.extradata_size),
+                        kCFAllocatorNull  // extradata will be deallocated by avformat_close_input()
+                    )
+                ] as CFDictionary
+        case AV_CODEC_ID_HEVC:
+            // assume this works like above?
+            atoms =
+                [
+                    "hvcC" as CFString: CFDataCreateWithBytesNoCopy(
+                        kCFAllocatorDefault,
+                        params.extradata,
+                        CFIndex(params.extradata_size),
+                        kCFAllocatorNull  // extradata will be deallocated by avformat_close_input()
+                    )
+                ] as CFDictionary
+        default:
+            // TODO: "av1C" https://aomediacodec.github.io/av1-isobmff/#av1codecconfigurationbox-section
+            // TODO: "vpcC" https://www.webmproject.org/vp9/mp4/#vp-codec-configuration-box
+            if params.extradata_size != 0 {
+                let hex = UnsafeBufferPointer(start: params.extradata, count: Int(params.extradata_size)).reduce("data=", { result, byte in String(format: "%@ %02x", result, byte) })
                 logger.debug(
                     "loadTrackInfo unhandled extradata \(params.extradata_size) bytes with codec \"\(FormatReader.avcodec_name(params.codec_id), privacy:.public)\": \(hex, privacy:.public)"
                 )
